e3598720a72ac5507ab89ee68dfcdc3fea9f664b
github username <github email address>
2019-04-15 22:05:54 -0400

    testing

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
 
   get "/get_user", to: "users#get_user"
   post "/login_user", to: "auth#create"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
